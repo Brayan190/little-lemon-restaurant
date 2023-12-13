75321d0a1afa3d6b0c656f6f00e71caa559980ff
User <bryan07arias12@gmail.com>
2023-12-12 19:54:38 -0700

    styles in navbar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import "./css/global.css"
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
